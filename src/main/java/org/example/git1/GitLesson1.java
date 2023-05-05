08d0091d3c3fa560c86ed07db7b6300976268e24
ahmadovht <huseyn.ahmadov@kapitalbank.az>
2023-05-06 00:53:46 +0400

    added message for develop branch

--- src/main/java/org/example/git1/GitLesson1.java
+++ src/main/java/org/example/git1/GitLesson1.java
@@ -4,5 +4,6 @@ public class GitLesson1 {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("This code is belongs to Develop branch");
     }
 }
