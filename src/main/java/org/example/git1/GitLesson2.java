abc197ac7c6318e22a0ba8bb699bdba46a99dbe7
ahmadovht <huseyn.ahmadov@kapitalbank.az>
2023-05-06 01:03:55 +0400

    added new sout for merge task

---
+++ src/main/java/org/example/git1/GitLesson2.java
@@ -0,0 +1,13 @@
+package org.example.git1;
+
+public class GitLesson2 {
+
+    public static void main(String[] args) {
+        System.out.println("This is first message git lesson 1");
+        System.out.println("This is second message for git lesson 2");
+        System.out.println("This is third message for git lesson 3");
+        System.out.println("This is third message for git lesson 3");
+        System.out.println("This is fourth message for git lesson 3");
+    }
+
+}
