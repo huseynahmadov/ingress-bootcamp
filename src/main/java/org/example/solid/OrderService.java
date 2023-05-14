ea8cabbc054eadb501fca9753bfb9368d569c9f8
huseynahmadov <t.huseyn96@mail.ru>
2023-05-15 00:09:21 +0400

    open closed added

---
+++ src/main/java/org/example/solid/OrderService.java
@@ -0,0 +1,13 @@
+package org.example.solid;
+
+public class OrderService {
+
+    public void saveOrder() {
+        System.out.println("Order saved to DB");
+    }
+
+    public void saveOrderToCloud() {
+        System.out.println("Order saved to cloud");
+    }
+
+}
